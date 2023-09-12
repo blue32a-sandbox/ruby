954e2911228c81508dc556723b6419cac03f9f44
blue32a <blue32a.jp@gmail.com>
2023-09-12 12:36:32 +0000

    変数と定数

---
+++ src/tutorial/020_data_type1/variables_and_constants.rb
@@ -0,0 +1,15 @@
+a = 100
+b = 200
+c = a + b
+puts c # 300
+
+c = "Ruby"
+puts c # Ruby
+
+CONST = "Ruby"
+puts CONST
+
+# warning: already initialized constant CONST
+# CONST = "Java"
+
+puts RUBY_VERSION
