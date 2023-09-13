108efe78f0a276c28902642ae7cbceb31f716942
blue32a <blue32a.jp@gmail.com>
2023-09-13 10:10:10 +0000

    条件が成立する間（しない間）繰り返し

---
+++ src/tutorial/050_control_flow/loop.rb
@@ -0,0 +1,10 @@
+num = 0
+
+loop {
+  num += 1
+  break if num == 4
+  puts "num = #{num}"
+}
+# num = 1
+# num = 2
+# num = 3
---
+++ src/tutorial/050_control_flow/while.rb
@@ -0,0 +1,18 @@
+num = 1
+
+while num < 5 do
+  puts "num = #{num}"
+  num = num + 1
+end
+# num = 1
+# num = 2
+# num = 3
+# num = 4
+
+until num < 3 do
+  puts "num = #{num}"
+  num = num - 1
+end
+# num = 5
+# num = 4
+# num = 3
