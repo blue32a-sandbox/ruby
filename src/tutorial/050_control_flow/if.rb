45be9482223b95858863869eac9f8ca2e0143285
blue32a <blue32a.jp@gmail.com>
2023-09-13 09:27:09 +0000

    if文

---
+++ src/tutorial/050_control_flow/if.rb
@@ -0,0 +1,12 @@
+a = 100
+b = 200
+
+if a > 100 && b.zero? then
+  puts "a is greater than 100 and b is 0"
+elsif a > 100 then
+  puts "a is greater than 100 and b is not 0"
+elsif a == 100 then
+  puts "a is equal to 100"
+else
+  puts "a is smaller than 100"
+end
